fae8b7160bfa512d6fbe674b9baf6d285ef915fc
pau-raurich <79652592+pau-raurich@users.noreply.github.com>
2021-11-28 10:38:58 +0100

    Initial commit

---
+++ app/src/main/java/cat/dam/pau/dia_nit/MainActivity.java
@@ -0,0 +1,14 @@
+package cat.dam.pau.dia_nit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
